2fd64f3e4f3bde26d5f48c3dd5468d3e7954eac1
Adam Johnson <adam.b.johnson1@gmail.com>
2018-06-01 10:10:04 -0400

    getElementByID doesn't return an array.

--- javascripts/dist/catalog.js
+++ javascripts/dist/catalog.js
@@ -988,15 +988,15 @@ var navigation = responsiveNav(".nav-collapse", {
 // Required JS for "WVU Masthead w/Nav + Search Toggle"
 
 // First, get the ID's of what we want to add classes to:
-var inactiveSearch  = document.getElementById('js-wvu-nav__search-toggle');
+var inactiveSearch = document.getElementById('js-wvu-nav__search-toggle');
 var mastheadSearchSlider = document.getElementById('js-wvu-masthead__search-slider');
 
 // On click toggle the class `.active-search` to the search button. This changes it to an `×`
 // On click toggle the class `.closed` to the search. This pulls it from being offscreen into view.
-inactiveSearch.addEventListener('click', function(){
+inactiveSearch.addEventListener('click', function () {
   inactiveSearch.classList.toggle('active-search');
   mastheadSearchSlider.classList.toggle('closed');
-}, false);
+});
 
 // Accessible Tabs
 // https://codepen.io/BeyondHyper/pen/xZXXzj
