49b3658e26db68889e73e18157092e15fc8bca29
Adam Johnson <adam.b.johnson1@gmail.com>
2018-06-01 14:21:13 -0400

    Refactor the search toggle JS.
    This update makes the classes less specific, more semantic and sets focus to the appropriate element (search when it's active, the button after it get's clicked again to close).

--- javascripts/dist/catalog.js
+++ javascripts/dist/catalog.js
@@ -990,12 +990,27 @@ var navigation = responsiveNav(".nav-collapse", {
 // First, get the ID's of what we want to add classes to:
 var inactiveSearch = document.getElementById('js-wvu-nav__search-toggle');
 var mastheadSearchSlider = document.getElementById('js-wvu-masthead__search-slider');
+var mastheadSearchInput = document.getElementById('js-wvu-masthead__search-input');
 
-// On click toggle the class `.active-search` to the search button. This changes it to an `×`
-// On click toggle the class `.closed` to the search. This pulls it from being offscreen into view.
+// Find the search toggle, then, on click
+// If there is not a class of `wvu-nav__custom-search-toggle--active-search`, add it. This changes the icon to an `×`
+// At the same time, make the search pattern visible by adding the `wvu-masthead__search-slider--is-visible` class to the search container.
+// Finally, add focus to the <input>.
 inactiveSearch.addEventListener('click', function () {
-  inactiveSearch.classList.toggle('active-search');
-  mastheadSearchSlider.classList.toggle('closed');
+  if (!this.classList.contains('wvu-nav__custom-search-toggle--active-search')) {
+    this.classList.add('wvu-nav__custom-search-toggle--active-search');
+    mastheadSearchSlider.classList.add('wvu-masthead__search-slider--is-visible');
+    mastheadSearchInput.focus();
+  }
+  // When the button has already been clicked (and has the class we added above), the search is visible.
+  // Remove the `wvu-nav__custom-search-toggle--active-search` to change the `×` icon back to a search icon.
+  // Also remove the `wvu-masthead__search-slider--is-visible` to hide the search.
+  // Then, finally, restore focus to the search button.
+  else {
+    this.classList.remove('wvu-nav__custom-search-toggle--active-search');
+    mastheadSearchSlider.classList.remove('wvu-masthead__search-slider--is-visible');
+    this.focus();
+  }
 });
 
 // Accessible Tabs
